327d1ff45331fe200ff1f387641ca69dcf742abc
Manoel Carvalho <manoelcarvalho1984@gmail.com>
2022-08-26 19:14:17 -0300

    first commit

---
+++ src/br/com/carvalho/stream/AppList.java
@@ -0,0 +1,15 @@
+package br.com.carvalho.stream;
+
+
+
+public class AppList {
+
+	public static void main(String[] args) {
+		
+		ListPeople list = new ListPeople();
+		list.generoSexo();
+		
+		
+	}
+
+}
